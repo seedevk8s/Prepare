c92616da10a3e46311a398b7ee1ddd718a6cbbba
seedevk8s <seedevk8s@gmail.com>
2020-08-25 17:29:50 +0900

    Repository 기능 확인하기 위한 테스트 클래스 추가

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+
+public class UserRepositoryTest extends StudyApplicationTests {
+
+    public void create(){
+
+    }
+
+    public void read() {
+
+    }
+}
