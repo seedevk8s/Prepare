b2c3ec36305b1af83834ad5997e13f8548f0195b
seedevk8s <seedevk8s@gmail.com>
2020-08-25 16:38:27 +0900

    Entity 설정 추가

--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -1,8 +1,15 @@
 package com.example.study.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class User {
 
