4aef9a65d000beada0adfc193e80100819812a4f
seedevk8s <seedevk8s@gmail.com>
2020-08-25 16:27:49 +0900

    Entity 추가

---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.study.model.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
+}
