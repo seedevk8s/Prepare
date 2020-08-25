fa94c79b7eee0ea751bb06e30baab7d7324df993
seedevk8s <seedevk8s@gmail.com>
2020-08-25 12:24:02 +0900

    Lombok 라이브러리 dependency에 추가함

--- src/main/java/com/example/study/model/SearchParam.java
+++ src/main/java/com/example/study/model/SearchParam.java
@@ -6,40 +6,5 @@ public class SearchParam {
     private String email;
     private int page;
 
-    public SearchParam() {
-    }
 
-    public SearchParam(String account) {
-        this.account = account;
-    }
-
-    public SearchParam(String account, String email, int page) {
-        this.account = account;
-        this.email = email;
-        this.page = page;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
 }
